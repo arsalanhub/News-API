84c9bbe083a3c4689bdf20aaefc21dd80da48de9
Mohammad Arsalan <arsalanmohd237@gmail.com>
2022-09-28 22:14:04 +0530

    route configurations

--- news_app/views.py
+++ news_app/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import decorators, permissions
+from rest_framework.response import Response
 
-# Create your views here.
+@decorators.api_view(['GET'])   
+@decorators.permission_classes([permissions.AllowAny])
+def getEntertainmentNews(request):
+    print("Inside Entertainment Function")
