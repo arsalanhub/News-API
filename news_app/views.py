99189f7cd4594cab04fb2d4e5fa123e040823c2d
Mohammad Arsalan <arsalanmohd237@gmail.com>
2022-09-28 23:50:57 +0530

    entertainment section added

--- news_app/views.py
+++ news_app/views.py
@@ -1,7 +1,20 @@
 from rest_framework import decorators, permissions
 from rest_framework.response import Response
+from bs4 import BeautifulSoup
+import urllib.request
 
 @decorators.api_view(['GET'])   
 @decorators.permission_classes([permissions.AllowAny])
 def getEntertainmentNews(request):
-    print("Inside Entertainment Function")
+    response = urllib.request.urlopen("https://indianexpress.com/section/entertainment/page/3/")
+    page_source = response.read()
+
+    soup = BeautifulSoup(page_source, "html.parser")
+
+    data = soup.find_all("div", class_="title")
+    dataArr = []
+    for i in data:
+        dataDict = dict()
+        dataDict["title"] = i.a.text
+        dataArr.append(dataDict)
+    return Response(dataArr, 200)
