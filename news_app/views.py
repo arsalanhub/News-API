de7f16879a130dbfb5113ebef64883f27c9ca09d
Mohammad Arsalan <arsalanmohd237@gmail.com>
2022-09-29 00:24:34 +0530

    image and body section added

--- news_app/views.py
+++ news_app/views.py
@@ -6,15 +6,17 @@ import urllib.request
 @decorators.api_view(['GET'])   
 @decorators.permission_classes([permissions.AllowAny])
 def getEntertainmentNews(request):
-    response = urllib.request.urlopen("https://indianexpress.com/section/entertainment/page/3/")
+    response = urllib.request.urlopen("https://indianexpress.com/section/entertainment/page/1/")
     page_source = response.read()
 
     soup = BeautifulSoup(page_source, "html.parser")
 
-    data = soup.find_all("div", class_="title")
+    data = soup.find_all("div", class_="articles")
     dataArr = []
     for i in data:
         dataDict = dict()
-        dataDict["title"] = i.a.text
+        dataDict["title"] = i.div.find_next_sibling("div").a.text
+        dataDict["image"] = i.div.a.img["data-lazy-src"]
+        dataDict["body"] = i.p.text
         dataArr.append(dataDict)
     return Response(dataArr, 200)
