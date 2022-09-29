118948f7dfede58ac17e844b7af5fa715e20f411
Mohammad Arsalan <arsalanmohd237@gmail.com>
2022-09-29 08:28:02 +0530

    get data from 2 page

--- news_app/views.py
+++ news_app/views.py
@@ -6,17 +6,19 @@ import urllib.request
 @decorators.api_view(['GET'])   
 @decorators.permission_classes([permissions.AllowAny])
 def getEntertainmentNews(request):
-    response = urllib.request.urlopen("https://indianexpress.com/section/entertainment/page/1/")
-    page_source = response.read()
-
-    soup = BeautifulSoup(page_source, "html.parser")
-
-    data = soup.find_all("div", class_="articles")
     dataArr = []
-    for i in data:
-        dataDict = dict()
-        dataDict["title"] = i.div.find_next_sibling("div").a.text
-        dataDict["image"] = i.div.a.img["data-lazy-src"]
-        dataDict["body"] = i.p.text
-        dataArr.append(dataDict)
+    for pageNumber in range(1, 3, 1):
+        response = urllib.request.urlopen("https://indianexpress.com/section/entertainment/page/{}/".format(pageNumber))
+        page_source = response.read()
+
+        soup = BeautifulSoup(page_source, "html.parser")
+
+        data = soup.find_all("div", class_="articles")
+
+        for i in data:
+            dataDict = dict()
+            dataDict["title"] = i.div.find_next_sibling("div").a.text
+            dataDict["image"] = i.div.a.img["data-lazy-src"]
+            dataDict["body"] = i.p.text
+            dataArr.append(dataDict)
     return Response(dataArr, 200)
